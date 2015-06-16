dd9092ec3f9fe1a898aeabdab1a6d166f55f073c
robotmayo <robotmayo@gmail.com>
2015-06-16 17:19:54 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+var config = require('./config');
+var Sandbox = new (require('sandbox'));
+
+
+
+
+server.connection({ port: 3000 });
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
+
+server.route({
+    method: 'POST',
+    path: '/slack/outgoing/eval-js',
+    handler: function (request, reply) {
+        if(request.payload.token !== config.slackWebhookToken) return reply('Incorrect token').code(401);
+        var code = request.payload.text.slice(3).trim();
+        Sandbox.run(code, function(out){
+            var response = '';
+            if( (!out.result || out.result === 'null') && out.console.length === 0 ) return reply('Snippet ran sucessfully.');
+            if(out.console.length > 0){
+                response += out.console.reduce(function(s, c){
+                    return s + c.replace('\n', ' ');
+                }, '');
+                return reply(response);
+            }
+            response = out.result.split('\n').slice(0,3).join('\n');
+            reply(response);
+        });
+    }
+});
